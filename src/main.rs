0b042b67a7d7bce9a10a1669c1d066bed5a88728
LowFreeKey <lowfreekey@gmail.com>
2022-03-22 19:42:46 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn fizzTest() {
+    for i in 1..102 {
+        match i {
+            i if (i % 15 == 0) => { println!("{:?}", "FizzBuzz") },
+            i if (i % 3 == 0) => { println!("{:?}", "Fizz") },
+            i if (i % 5 == 0) => { println!("{:?}", "Buzz") },
+            _ => { println!("{:?}", i) },
+        }
+    }
+}
